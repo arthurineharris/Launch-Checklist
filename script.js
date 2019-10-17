b95be5d9c9670e87187d31e44b294c7d2c1133e1
Arthurine Harris <Arthurineharris@yahoo.com>
2019-10-16 20:06:22 -0500

    Started Project

--- script.js
+++ script.js
@@ -1,5 +1,40 @@
 // Write your JavaScript code here!
 
+window.addEventListener("load", function() {
+      let form = document.querySelector("form");
+      form.addEventListener("submit", function(event) {
+         let pilotNameInput = document.querySelector("input[name=pilotName]");
+         let copilotName = document.querySelector("input[name=copilotName]");
+         let fuelLevel = document.querySelector("input[name=fuelLevel]");
+         let cargoMass = document.querySelector("input[name=cargoMass]");
+
+      if (pilotNameInput.value === "" || copilotName.value === "" || fuelLevel.value === "" ||
+         cargoMass.value === "") {
+            alert("All fields are required!");
+            event.preventDefault();
+            };
+      if (isNaN(cargoMass)) {
+            alert("Cargo Mass must be a number!");
+            event.preventDefault();
+            };
+      if (isNaN(fuelLevel)) {
+               alert("Fuel Level must be a number!");
+               event.preventDefault();
+         // };
+         // if (isNaN(pilotNameInput) === false) {
+         //       alert("Pilot Name cannot be a number!");
+         //       event.preventDefault();
+         // };
+         // if (isNan(copilotName) === false) {
+         //       alert("Co-pilot Name cannot be a number!");
+         //       event.preventDefault();
+         // };
+
+
+            
+         
+      });
+   });
 /* This block of code shows how to format the HTML once you fetch some planetary JSON!
 <h2>Mission Destination</h2>
 <ol>
