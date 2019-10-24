a10735c7e2eb0e7a5c0a7815d45ac78756cf16fe
Arthurine Harris <Arthurineharris@yahoo.com>
2019-10-23 22:50:33 -0500

    Fetched Planetary Data

--- script.js
+++ script.js
@@ -22,23 +22,21 @@ window.addEventListener("load", function() {
                alert("Fuel Level must be a number!");
                event.preventDefault();
          }
-      if (!isNaN(pilotNameInput.value)) {
+      if (!isNaN(pilotNameInput.value) && pilotNameInput.value!= "") {
                alert("Pilot Name cannot be a number!");
                event.preventDefault();
          }
-      if (!isNaN(copilotName.value)) {
+      if (!isNaN(copilotName.value) && copilotName.value!= "") {
                alert("Co-pilot Name cannot be a number!");
                event.preventDefault();
          }
-
-      // })
-//  });            
+            
  
  let pilotStatus = document.getElementById("pilotStatus").innerHTML = (`Pilot ${pilotNameInput.value} is ready for launch.`) ;
 
  let copilotStatus = document.getElementById("copilotStatus").innerHTML = (`Co-pilot ${copilotName.value} is ready for launch.`);
  
- if (fuelLevel.value < 10000) {
+ if (fuelLevel.value < 10000 && fuelLevel.value != "") {
     let faultyItems = document.getElementById("faultyItems");
     faultyItems.style.visibility = 'visible';
     event.preventDefault();
@@ -62,7 +60,14 @@ window.addEventListener("load", function() {
     event.preventDefault();
  }
   
- if (cargoMass.value <= 10000 && fuelLevel.value >= 10000) {
+ function noEmptyFields(){
+   if (pilotNameInput.value === "" || copilotName.value === "" || fuelLevel.value === "" ||
+   cargoMass.value === "") {
+      return false;
+   }
+ }
+
+ if (cargoMass.value <= 10000 && fuelLevel.value >= 10000 && noEmptyFields()) {
    let faultyItems = document.getElementById("faultyItems");
    faultyItems.style.visibility = 'visible';
 
@@ -72,7 +77,25 @@ window.addEventListener("load", function() {
 
 })  
 
+fetch("https://handlers.education.launchcode.org/static/planets.json").then( function(response) {
+   response.json().then( function(json) {
+      const div = document.getElementById("missionTarget");
+      div.innerHTML = `
+      <h2>Mission Destination</h2>
+      <ol>
+        <li>Name: ${json[0].name}</li>
+        <li>Diameter: ${json[0].diameter}</li>
+        <li>Star: ${json[0].star}</li>
+        <li>Distance from Earth: ${json[0].distance}</li>
+        <li>Number of Moons: ${json[0].moons}</li>
+      </ol>
+      <img src="${json[0].image}">
+      `;
+   })
+})
+
 });
+
 // This block of code shows how to format the HTML once you fetch some planetary JSON!
 // <h2>Mission Destination</h2>
 // <ol>
